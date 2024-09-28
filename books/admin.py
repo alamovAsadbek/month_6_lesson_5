ffe62ff608903c24b3697be896cc755eef6e0223
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-28 16:33:23 +0500

    change

--- books/admin.py
+++ books/admin.py
@@ -6,3 +6,9 @@ from books.models import BookModel
 @admin.register(BookModel)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('id', 'image', 'name', 'description', 'author', 'price', 'category', 'created_at', 'updated_at')
+    list_display_links = ('id', 'image', 'name', 'description', 'author', 'price', 'category', 'created_at',
+                          'updated_at')
+    list_filter = ('category', 'created_at', 'updated_at')
+    search_fields = ('name', 'description', 'author', 'price', 'category')
+    list_per_page = 20
+    readonly_fields = ('created_at', 'updated_at')
