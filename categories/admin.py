f3337c207b15633e76a9bc5873089fb681877485
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-28 16:17:39 +0500

    change admin.py

--- categories/admin.py
+++ categories/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from categories.models import CategoryModel
 
-admin.site.register(CategoryModel)
+
+@admin.register(CategoryModel)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    list_display_links = ('name', 'description', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')
