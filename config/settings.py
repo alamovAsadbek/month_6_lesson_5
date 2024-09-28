774cb4ea922388c715101a8288579f342bdf4d1d
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-28 16:21:37 +0500

    change settings.py

--- config/settings.py
+++ config/settings.py
@@ -87,10 +87,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-mediaroot = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = mediaroot
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -105,7 +101,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'assets',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
