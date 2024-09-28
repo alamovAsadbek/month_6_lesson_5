7e782820ec928a92335b8d7782aedb84e876f83c
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-28 15:59:16 +0500

    change

--- categories/models.py
+++ categories/models.py
@@ -11,3 +11,7 @@ class CategoryModel(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        verbose_name = "Category"
