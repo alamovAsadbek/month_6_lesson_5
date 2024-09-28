449e00a0c526c04582db23fc1ef55bde065f7ee6
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-28 16:24:38 +0500

    change books/models.py

--- books/models.py
+++ books/models.py
@@ -3,3 +3,13 @@ from django.db import models
 
 class BookModel(models.Model):
     image = models.ImageField(upload_to='books/images/')
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
