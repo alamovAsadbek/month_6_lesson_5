da50345dfb0797d255fdfc8cade9903b39465a4c
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-28 15:58:43 +0500

    change

--- categories/models.py
+++ categories/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 
 class CategoryModel(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, auto_created=name)
+    description = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
