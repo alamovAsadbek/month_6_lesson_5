e8f8ee7fa1e8b978969791f69e46fd66e7e780c5
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-28 16:32:26 +0500

    change books/admin.py

--- books/admin.py
+++ books/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from books.models import BookModel
 
-admin.site.register(BookModel)
+
+@admin.register(BookModel)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'name', 'description', 'author', 'price', 'category', 'created_at', 'updated_at')
