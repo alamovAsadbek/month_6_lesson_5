4c926e9217f016a961de234283d38d755a5c8df4
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-30 01:34:47 +0500

    change urls.py

--- books/urls.py
+++ books/urls.py
@@ -5,5 +5,5 @@ from books.views import booksView, bookDetailView, createBookView
 urlpatterns = [
     path('', booksView),
     path('<int:book_id>/', bookDetailView),
-    path('add/', createBookView),
+    path('add/', createBookView, name='create_book'),
 ]
