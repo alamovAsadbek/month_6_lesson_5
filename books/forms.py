c6b943f1516d6152124f72626e4204741a903cf5
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-30 01:32:23 +0500

    change

--- books/forms.py
+++ books/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from books.models import BookModel
 
 
-class BookModeForm(forms.ModelForm):
+class BookModelForm(forms.ModelForm):
     class Meta:
         model = BookModel
         fields = '__all__'
