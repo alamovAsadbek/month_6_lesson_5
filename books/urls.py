c84192e126d93df3570f4a9c768be1c65c1654ee
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-30 09:05:46 +0500

    change

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from books.views import booksView, bookDetailView, createBookView, updateBookView
+from books.views import booksView, bookDetailView, createBookView, updateBookView, deleteBookView
 
 urlpatterns = [
     path('', booksView),
     path('<int:book_id>/', bookDetailView, name='book_detail'),
     path('add/', createBookView, name='create_book'),
     path('edit/<int:book_id>/', updateBookView, name='edit_book'),
+    path('delete/<int:book_id>/', deleteBookView, name='delete_book'),
 ]
